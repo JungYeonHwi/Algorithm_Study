1c593013eaedce7e975038ea7e9fc59791446390
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2021-06-27 01:31:40 +0900

    10952.c

---
+++ 10952/10952/10952.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main() {
+	int x, y;
+
+	while(1) {
+		scanf("%d %d", &x, &y);
+		if (x == 0 && y == 0) break;
+		else printf("%d\n", x + y);
+	}
+
+	return 0;
+}
