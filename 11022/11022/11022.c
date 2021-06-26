98ed269ed99cbcd2b0f65d72c2d9ea20a7f4a0d8
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2021-06-27 01:04:28 +0900

    11022.c

---
+++ 11022/11022/11022.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#include <stdio.h>
+
+int main() {
+	int input;
+	int x, y;
+
+	scanf("%d", &input);
+
+	for (int i = 1; i <= input; i++) {
+		scanf("%d %d", &x, &y);
+		printf("Case #%d: %d + %d = %d\n", i,x, y, x + y);
+	}
+
+	return 0;
+}
