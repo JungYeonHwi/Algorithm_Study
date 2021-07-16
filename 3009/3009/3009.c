573ccd462df281504114a1e4f1ae566b9a446db7
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2021-07-17 00:50:08 +0900

    3009.c

---
+++ 3009/3009/3009.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main() {
+
+	int x1, y1, x2, y2, x3, y3;
+	scanf("%d %d", &x1, &y1);
+	scanf("%d %d", &x2, &y2);
+	scanf("%d %d", &x3, &y3);
+
+	int x, y;
+
+	if (x1 == x2) x = x3;
+	if (x2 == x3) x = x1;
+	if (x1 == x3) x = x2;
+
+	if (y1 == y2) y = y3;
+	if (y2 == y3) y = y1;
+	if (y1 == y3) y = y2;
+
+	printf("%d %d", x, y);
+
+
+	return 0;
+}
