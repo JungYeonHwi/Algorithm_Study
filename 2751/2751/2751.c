636c743d874f377d6c03b5f9e2ab36cf85e4d779
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2021-07-17 18:36:18 +0900

    2751.c

---
+++ 2751/2751/2751.c
@@ -0,0 +1,60 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+void swap(int* arr, int x, int y)
+{
+	int temp = arr[x];
+	arr[x] = arr[y];
+	arr[y] = temp;
+}
+
+void heap(int* arr, int n)
+{
+	int count = n;
+
+	int p, c;
+	for (int i = 1; i < count; i++) {
+		c = i;
+		while (c > 0) {
+			p = (c - 1) / 2;
+			if (arr[p] < arr[c]) {
+				swap(arr, p, c);
+				c = p;
+			}
+			else break;
+		}
+	}
+	swap(arr, 0, --count);
+
+	while (count > 1) {
+
+		p = 0;
+		while (p <= (count / 2) - 1) {
+			c = p * 2 + 1;
+			if (c + 1 < count && arr[c] < arr[c + 1])
+				c++;
+			if (arr[p] < arr[c]) {
+				swap(arr, p, c);
+				p = c;
+			}
+			else break;
+		}
+		swap(arr, 0, count-- - 1);
+	}
+}
+
+int main() {
+
+    int N;
+    scanf("%d", &N);
+
+    int* arr = (int*)malloc(sizeof(int) * N);
+
+    for (int i = 0; i < N; i++) scanf("%d", &arr[i]);
+
+	heap(arr, N);
+
+    for (int i = 0; i < N; i++) printf("%d", arr[i]);
+
+    return 0;
+}
