82dc2dfa940e39c93e51bd3d7d5317ca97b8a136
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-01-11 14:51:33 +0900

    1453.c

--- 1453/1453/1453.c
+++ 1453/1453/1453.c
@@ -3,24 +3,35 @@
 
 int main() {
 
-	int N;
-	scanf("%d", &N);
+	int a[100][100];
+	int b[100][100];
+	int sum[100][100];
+	int n, m;
+	scanf("%d %d", &n, &m);
 
-	int count = 0;
-
-	int sit[101] = { 0 };
-
-	int num;
-
-	for (int i = 0; i < N; i++) {
-		scanf("%d", &num);
-		if (sit[num] == 1)
-			count++;
-		sit[num] = 1;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			scanf("%d", &a[i][j]);
+		}
 	}
-
-	printf("%d", count);
-
+	for (int k = 0; k < n; k++)
+	{
+		for (int l = 0; l < m; l++)
+		{
+			scanf("%d", &b[k][l]);
+		}
+	}
+	for (int q = 0; q < n; q++)
+	{
+		for (int w = 0; w < m; w++)
+		{
+			sum[q][w] = a[q][w] + b[q][w];
+			printf("%d ", sum[q][w]);
+		}
+		printf("\n");
+	}
 	return 0;
 
 }
