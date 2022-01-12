df9a0f5f3724ea31807f758ddd7bf5e680afb697
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-01-13 01:43:39 +0900

    middle_letter

--- programmers_python/middle_letter.py
+++ programmers_python/middle_letter.py
@@ -1,1 +1,10 @@
-def
+def solution(s):
+    answer = ''
+    if len(s) % 2 == 1 : 
+        index = int(len(s) / 2) + 1
+        answer = s[index-1]
+    else : 
+        index = int(len(s) / 2)
+        answer = s[index-1:index+1]
+
+    return answer
