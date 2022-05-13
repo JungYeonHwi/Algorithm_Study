8b2f804015cd0723bc327b242dad151387996390
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-05-14 00:22:42 +0900

    1436 brute force

--- BaekJoon_python/1436.py
+++ BaekJoon_python/1436.py
@@ -2,10 +2,9 @@ n = int(input())
 six = 666
 count = 0
 
-while 1:
+while 1 :
     if '666' in str(six) : 
         count += 1
-        print(str(six))            
     if count == n :          
         print(six)          
         break               
