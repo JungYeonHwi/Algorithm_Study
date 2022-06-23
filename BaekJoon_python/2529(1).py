a7a4dba395ebf7ba14154238fd06186f7a069bd5
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-06-23 18:36:35 +0900

    2529 수정

--- BaekJoon_python/2529(1).py
+++ BaekJoon_python/2529(1).py
@@ -1,26 +1,26 @@
-from itertools import permutations
+from itertools import permutations # 순열 라이브러리 이용
 
-n = int(input())
-signs = input().split()
-nums = [0,1,2,3,4,5,6,7,8,9]
+n = int(input()) # 부등호 문자 개수
+signs = input().split() # 부등호 저장
+nums = [0,1,2,3,4,5,6,7,8,9] # 순열을 이용할 숫자 list
 
-permute = permutations(nums, n+1)
-result = []
+permute = permutations(nums, n+1) # 부등호는 가운데 있기 때문에 순열은 부등호 개수보다 1개 더 많이 하여 순열 표현
+result = [] # 정답을 저장할 list
 
-for per in permute :
-  flag = True
-  for i in range(len(signs)) :
-    if signs[i] == '<' :
-      if per[i] < per[i+1] : continue
-      else : 
-        flag = False
-        break
-    else :
-      if per[i] > per[i+1] : continue
-      else : 
-        flag = False
-        break
-  if flag : result.append(per)
+for per in permute : # 순열 조합들 모두 확인
+  flag = True # 부등호에 맞는 것인지 확인하기 위한 변수
+  for i in range(len(signs)) : # 부등호 길이만큼 확인
+    if signs[i] == '<' : # 부등호가 '<' 라면
+      if per[i] < per[i+1] : continue # 순열 조합도 '<' 확인하여 맞다면 계속
+      else : # 틀리다면
+        flag = False # 틀렸다고 표시
+        break # 멈춤
+    else : # 부등호가 '>' 라면
+      if per[i] > per[i+1] : continue # 순열 조합도 '>' 확인하여 맞다면 계속
+      else : # 틀리다면 
+        flag = False # 틀렸다고 표시
+        break # 멈춤
+  if flag : result.append(per) # 틀리지 않았다면 해당 순열을 저장
 
-print(''.join(map(str,list(max(result)))))
-print(''.join(map(str,list(min(result)))))
+print(''.join(map(str,list(max(result))))) # 가장 큰 숫자의 순열
+print(''.join(map(str,list(min(result))))) # 가장 작은 숫자의 순열
--- BaekJoon_python/2529(2).py
+++ BaekJoon_python/2529(2).py
@@ -1,31 +1,31 @@
-def numCheck(x, y, op) :
-  if op == '<' :
-    if x > y : return False
-  if op == '>' :
-    if x < y : return False
-  return True
+def numCheck(x, y, op) : # 부등호 기호를 확인할 함수
+  if op == '<' : # 부등호가 '<' 라면
+    if x > y : return False # 숫자들이 '>' 가 되면 false
+  if op == '>' : # 부등호가 '<' 라면
+    if x < y : return False # 숫자들이 '<' 가 되면 false
+  return True # 부등호 기호에 맞다면 true
 
-def re(index, num) :
+def re(index, num) : # 숫자들을 확인할 재귀 함수
   if index == n+1 : # 부등호가 n개 입력되니까 숫자는 n+1개 필요
-    result.append(num)
-    return
+    result.append(num) # 숫자가 n+1개가 되면 결과 저장
+    return # 종료
   
-  for i in range(10) :
+  for i in range(10) : # 숫자들 확인
     if check[i] : continue # 해당 숫자를 이미 사용했다면 pass
 
-    if index == 0 or numCheck(num[index-1], str(i), signs[index-1]) :
-      check[i] = True
-      re(index + 1, num+str(i))
-      check[i] = False
+    if index == 0 or numCheck(num[index-1], str(i), signs[index-1]) : # 숫자 개수가 0개 또는 부등호 기호들에 맞게 확인해서 true라면
+      check[i] = True # 해당 숫자들을 사용함을 표시
+      re(index + 1, num+str(i)) # 반복
+      check[i] = False # 숫자들 사용함 취소
 
-n = int(input())
-signs = input().split()
+n = int(input()) # 부등호 문자 개수
+signs = input().split() # 부등호 저장
 
-result = []
+result = [] # 정답을 저장할 list
 check = [False] * 10 # 해당 숫자를 사용했는지 안 했는지 체크
 
-re(0, '')
+re(0, '') # 재귀 함수 시작
 
-result.sort()
-print(result[-1])
-print(result[0])
+result.sort() # 정답 list를 정렬
+print(result[-1]) # list의 [-1]은 제일 큰 값
+print(result[0]) # list의 [0]은 제일 작은 값
