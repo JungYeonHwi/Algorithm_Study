0a5403b56803a6c90dc79a5e78785934f6c73ad7
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-06-24 15:15:22 +0900

    2193

---
+++ BaekJoon_python/2193.py
@@ -0,0 +1,11 @@
+import sys
+
+n = int(sys.stdin.readline().strip())
+dp = [[0 for _ in range(2)] for _ in range(n+1)]
+dp[1][1] = 1
+
+for i in range(2, n+1):
+    dp[i][0] = dp[i-1][0] + dp[i-1][1]
+    dp[i][1] = dp[i-1][0]
+
+print(sum(dp[n]))
