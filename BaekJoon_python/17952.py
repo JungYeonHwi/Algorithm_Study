d7b235aad6d9b74a1d6e680ad48a7b1aec9086e8
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-07-25 16:36:33 +0900

    17952

--- BaekJoon_python/17952.py
+++ BaekJoon_python/17952.py
@@ -1,23 +1,21 @@
-from collections import deque
-
-stk = deque()
-time = deque()
-answer = deque()
-
+import sys
+input = sys.stdin.readline
+ 
 n = int(input())
-
-for i in range(n) :
-    x = list(map(int,input().split()))
-    
-    if x[0] == 1 :
-        stk.append(x[1])
-        time.append(x[2])
-    else :
-        pass 
-    if time :
-        time[-1] -= 1 
-        if time[-1] == 0 :
+score, time = [], []
+result = []
+ 
+for _ in range(n):
+    flag = list(map(int, input().split()))
+ 
+    if flag[0]:    # 입력이 1 ? ? 인 경우
+        score.append(flag[1])
+        time.append(flag[2])
+ 
+    if time:
+        time[-1] -= 1
+        if not time[-1]:
+            result.append(score.pop())
             time.pop()
-            answer.append(stk.pop())
-            
-print(sum(answer))
+ 
+print(sum(result))
