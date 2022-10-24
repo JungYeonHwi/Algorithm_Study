82c733ecbcbdc430fad92f2628b15b278df7ff66
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-10-24 12:11:05 +0900

    next number.py

--- programmers_python/next number.py
+++ programmers_python/next number.py
@@ -4,4 +4,4 @@ def solution(common):
     if common[1] - common[0] == common[2] - common[1] : answer = common[-1] + common[1] - common[0]
     else : answer = common[-1] * common[1] // common[0]
     
-    return answernext number
+    return answer
