6e083625c8338a39ad600d98358da5f30723154f
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-10-25 21:15:44 +0900

    2563.py

---
+++ BaekJoon_python/2563.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+
+board = [[0 for _ in range(101)] for _ in range(101)]
+
+answer = 0
+
+n = int(input())
+
+for _ in range(n) : 
+    a, b = map(int, input().split())
+    
+    for i in range(a, a+10) : 
+        for j in range(b, b+10) : 
+            board[i][j] = 1
+            
+for row in board :
+    answer += row.count(1)
+    
+print(answer)
+
+    
