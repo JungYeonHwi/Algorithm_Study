b99c56e043234d27654c4d3c6492d64be0a1709e
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-10-25 21:01:44 +0900

    2566.py

--- BaekJoon_python/2566.py
+++ BaekJoon_python/2566.py
@@ -1,12 +1,21 @@
-num = 0
+import sys
+input = sys.stdin.readline
+
+board = []
+
+for _ in range(9) : 
+    board.append(list(map(int, input().split())))
+    
+x = 0
+y = 9
+maxValue = -1
 
 for i in range(9) : 
-    row = list(map(int, input().split()))
-    
-    if (max(row) > num) : 
-        num = max(row)
-        x = i + 1
-        y = row.index(num) + 1
-        
-print(num)
+    for j in range(9) : 
+        if board[i][j] > maxValue : 
+            maxValue = board[i][j]
+            x = i + 1
+            y = j + 1
+
+print(maxValue)
 print(x, y)
