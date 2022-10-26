6f2e0784c510ff915ac7730d64ee68e6da93df8f
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-10-26 22:13:44 +0900

    10819

--- BaekJoon_python/10819.py
+++ BaekJoon_python/10819.py
@@ -2,18 +2,20 @@ from itertools import permutations
 
 n = int(input())
 
-data = []
-
-for i in range(1, n+1) : 
-    data.append(i)
+data = list(map(int, input().split()))
     
 result = list(permutations(data, n))
+answer = []
 
 for i in range(0, len(result)) : 
     one = []
     for j in result[i] : 
         one.append(j)
         
-    for key in one : 
-        print(key, end=" ")
-    print()
+    s = []
+    for j in range(0, len(one)-1) : 
+        s.append(abs(one[j] - one[j+1]))
+        
+    answer.append(sum(s))
+    
+print(max(answer))
