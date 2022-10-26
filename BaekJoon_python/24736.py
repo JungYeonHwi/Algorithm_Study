1665cff4a4c91f0c9c78d5ddb811ddd77a336848
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-10-26 22:56:43 +0900

    24736

---
+++ BaekJoon_python/24736.py
@@ -0,0 +1,7 @@
+arr1 = list(map(int, input().split()))
+arr2 = list(map(int, input().split()))
+
+a = arr1[0] * 6 + arr1[1] * 3 + arr1[2] * 2 + arr1[3] * 1 + arr1[4] * 2
+b = arr2[0] * 6 + arr2[1] * 3 + arr2[2] * 2 + arr2[3] * 1 + arr2[4] * 2
+
+print(a, b)
