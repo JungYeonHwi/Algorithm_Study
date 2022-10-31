26dd743180c92f63a1473a9e853be2b2bdae708d
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-10-31 23:57:10 +0900

    14916

---
+++ BaekJoon_python/14916.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+count = 0
+
+num = n % 5
+
+if n == 1 or n == 3 : 
+    print(-1)
+    exit(0)
+elif num % 2 == 0 : 
+    print(n // 5 + num // 2)
+    exit(0)
+else : 
+    print((n // 5) - 1 + (num + 5) // 2)
+    exit(0)
