87a6330dc1a5debd66e0cee642a58ae8f2f41a62
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-05 01:41:20 +0900

    5430

--- BaekJoon_python/5430.py
+++ BaekJoon_python/5430.py
@@ -1,22 +1,30 @@
-import sys
 from collections import deque
-
+ 
 t = int(input())
-
-for i in range(t):
-    p = sys.stdin.readline().rstrip()
+ 
+for i in range(t) :
+    p = input()
     n = int(input())
-    arr = deque(sys.stdin.readline().rstrip()[1:-1].split(","))
-    
-    if n == 0 : arr = deque()
-    
+    arr = input()[1:-1].split(',')
+ 
+    queue = deque(arr)
+ 
     flag = 0
-    for j in p : 
-        if j == "R" : arr.reverse()
-        elif j == "D" :
-            if arr : arr.popleft()
-            else : 
+ 
+    if n == 0 : queue = []
+    for j in p :
+        if j == 'R' :
+            flag += 1
+        elif j == 'D' :
+            if len(queue) == 0 :
                 print("error")
-                flag = 1
                 break
-    if flag == 0 : print("["+",".join(arr)+"]")
+            else :
+                if flag % 2 == 0 : queue.popleft()
+                else : queue.pop()
+ 
+    else :
+        if flag % 2 == 0 : print("[" + ",".join(queue) + "]")
+        else : 
+            queue.reverse()
+            print("[" + ",".join(queue) + "]")
