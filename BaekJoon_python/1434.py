f4d27fbdc8ab6405a616c8c00d7a88dc6f9a99bc
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-05 17:57:11 +0900

    1434

---
+++ BaekJoon_python/1434.py
@@ -0,0 +1,14 @@
+N, M = map(int, input().split())
+box = list(map(int, input().split()))
+book = list(map(int, input().split()))
+i = j = t = inBox = 0
+
+while i < N and j < M :
+    if box[i] < t+book[j] :
+        t = 0
+        i += 1
+    else :
+        inBox += book[j]
+        t += book[j]
+        j += 1
+print(sum(box) - inBox)    
