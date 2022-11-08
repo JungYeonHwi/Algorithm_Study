d0fa63bf254cd6b9abd6e19db67a3b8c612cc939
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-08 10:13:50 +0900

    2511

---
+++ BaekJoon_python/2511.py
@@ -0,0 +1,23 @@
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+if A == B :
+    print(10, 10)
+    print("D")
+else :
+    a = b = 0
+    for i in range(10) :
+        if A[i] > B[i] :
+            a += 3
+            win = 'A'
+        elif A[i] < B[i] :
+            b += 3
+            win = 'B'
+        else :
+            a += 1
+            b += 1    
+    print(a, b)
+    if a == b : print(win)
+    else : 
+        if a > b : print('A')
+        else : print('B')
