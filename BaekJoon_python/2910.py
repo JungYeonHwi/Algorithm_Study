cb3bea47f4db6dbc5d011590d903b3d43e875287
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-08 16:57:44 +0900

    2910

---
+++ BaekJoon_python/2910.py
@@ -0,0 +1,15 @@
+n, c = map(int, input().split())
+numbers = list(map(int, input().split()))
+count = {}
+
+for idx, num in enumerate(numbers) :
+    if num in count : count[num][0] += 1
+    else : count[num] = [1, idx, num]
+
+frequency = sorted(count.values(), key= lambda x: (-x[0], x[1]))
+result = []
+
+for fre in frequency :
+    result += [fre[2]] * fre[0]
+    
+print(*result)
