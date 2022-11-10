10895fca5915fcf446593eae09195cdd8ff439ef
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-10 16:41:29 +0900

    25024

--- BaekJoon_python/25024.py
+++ BaekJoon_python/25024.py
@@ -1,7 +1,13 @@
-for i in range(1, int(input()) + 1) : 
-    a, b = map(int, input().split())
+for i in range(int(input())):
+    a,b=map(int,input().split())
     
-    res = a + b
-    if res >= 24 : res -= 24
+    print("Yes" if 0<=a<=23 and 0<=b<=59 else "No", end=" ")
     
-    print("#" + str(i) + res)
+    if a==2 :
+        print("Yes" if 1<=b<=29 else "No")
+    elif a in [1,3,5,7,8,10,12] :
+        print("Yes" if 1<=b<=31 else "No")
+    elif a in [4,6,9,11] :
+        print("Yes" if 1<=b<=30 else "No")
+    else :
+        print("No")
