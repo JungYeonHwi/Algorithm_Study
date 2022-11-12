319e42e81ff822b44d71dbc0552017b27ef0d434
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-13 00:27:03 +0900

    safety zone

---
+++ programmers_python/safety zone.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+def solution(board):
+    
+    dx = [-1, 1, 0 , 0, 1, 1, -1, -1]
+    dy = [0, 0, -1, 1, -1, 1, 1, -1]
+    
+    length = len(board)
+    visited = [[False] * length for _ in range(length)]
+
+    def BFS(x, y):
+        dq = deque()
+        dq.append((x, y))
+        while dq:
+            a, b = dq.popleft()
+            visited[a][b] = True
+            for i in range(8):
+                nx = dx[i] + a
+                ny = dy[i] + b
+                
+                if 0 <= nx < length and 0 <= ny < length and not visited[nx][ny] :
+                    if board[nx][ny] == 1 :
+                        dq.append((nx, ny))
+                    else :
+                        board[nx][ny] = 2
+
+    for i in range(length) :
+        for j in range(length) :
+            if board[i][j] == 1:
+                BFS(i, j)
+    result = 0
+    for i in range(length):
+        result += board[i].count(0)
+    return result
