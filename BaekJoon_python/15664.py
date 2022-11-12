8137e9d3f846244e107d4240456fe8a7e5f8ea13
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-12 21:36:26 +0900

    15664

---
+++ BaekJoon_python/15664.py
@@ -0,0 +1,16 @@
+from itertools import permutations, combinations, product, combinations_with_replacement
+
+n, m = map(int, input().split())
+k = sorted(list(map(int, input().split())))
+
+P = sorted(combinations(k, m))
+answer = []
+for p in P :
+    answer.append(p)
+    
+answer = sorted(list(set(answer)))
+
+for i in answer : 
+    for j in i :
+        print(j, end = " ")
+    print()
