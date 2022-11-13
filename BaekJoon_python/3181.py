6baff61955cae0a4b4a736d5ddc1d1cbb55d72ae
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-13 12:00:20 +0900

    3181

---
+++ BaekJoon_python/3181.py
@@ -0,0 +1,10 @@
+arr = ['i', 'pa', 'te', 'ni', 'niti', 'a', 'ali', 'nego', 'no', 'ili']
+
+b = input().split()
+r = b[0][0]
+
+for i in range(1,len(b)):
+    if b[i] in arr : continue
+    r += b[i][0]
+    
+print(r.upper())
