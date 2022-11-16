561fa0ff2238e38ca3e4424961fd50c4540cdc6d
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-17 00:00:23 +0900

    1007

---
+++ BaekJoon_python/1007.py
@@ -0,0 +1,40 @@
+import sys
+import math
+import itertools
+
+T = int(sys.stdin.readline())
+results = []
+ 
+for _ in range(T) :
+    N = int(sys.stdin.readline())
+    coords = []
+    
+    xTotal = 0
+    yTotal = 0
+    for _ in range(N):
+        x, y = map(int, sys.stdin.readline().split())
+        xTotal += x
+        yTotal += y
+        coords.append([x, y])
+    
+    res = math.inf
+    combinations = list(itertools.combinations(coords, int(N/2)))
+    l = int(len(combinations) / 2)
+    for s in combinations[:l] :
+        s = list(s)
+ 
+        x1Total = 0
+        y1Total = 0
+        for x1, y1 in s :
+            x1Total += x1
+            y1Total += y1
+        
+        x2Total = x1Total - x1Total
+        y2Total = y1Total - y1Total
+        
+        res = min(res, math.sqrt((x1Total - x2Total) ** 2 + (y1Total - y2Total) ** 2))
+ 
+    results.append(res)
+ 
+for result in results :
+    sys.stdout.write(str(result)+'\n')
