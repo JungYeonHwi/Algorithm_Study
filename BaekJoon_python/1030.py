c4c46123ad2cd188173a57c10ba70340980ed0ee
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-19 17:10:43 +0900

    1030

---
+++ BaekJoon_python/1030.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+def check(l, point):
+    x,y = point
+    center = l//N
+    if l == 1 : return 0
+    if center * (N-K)//2 <= x < center * (N+K)//2 and center * (N-K)//2 <= y < center * (N+K)//2 : return 1
+    x %= center
+    y %= center
+    return check(l//N, [x,y])
+
+s, N, K, R1, R2, C1, C2 = map(int,input().split())
+l = N ** s
+
+
+for i in range(R1, R2+1) :
+    for j in range(C1, C2+1) :
+        print(check(l,[i,j]), end="")
+    print()
