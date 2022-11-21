4131b3eee5775ca5dc590800cb53b354186b5b4b
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-21 22:47:11 +0900

    1940

---
+++ BaekJoon_python/1940.py
@@ -0,0 +1,20 @@
+n = int(input())
+m = int(input())
+arr = list(map(int, input().split()))
+
+arr.sort()
+cnt = 0
+
+i, j = 0, n-1
+
+while i < j : 
+    if arr[i] + arr[j] == m : 
+        cnt += 1
+        i += 1
+        j -= 1
+    elif arr[i] + arr[j] < m : 
+        i += 1
+    else : 
+        j -=1
+        
+print(cnt)
