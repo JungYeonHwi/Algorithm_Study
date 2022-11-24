1aff66870b5ea8f074e17a30a5c2341ab70249c4
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-24 15:31:53 +0900

    14920

--- BaekJoon_python/9094(시간초과).py
+++ BaekJoon_python/9094(시간초과).py
@@ -7,6 +7,6 @@ for _ in range(int(input())):
 
     for a in range(1, n - 1):
         for b in range(a + 1, n):
-            if (a**2 + b**2 + m) % (a*b) == 0:
-                count += 1
+            t = a * a + b * b + m
+            if t % (a * b) == 0 : count += 1
     print(count)
