cd00ba6ede799b6cce2bdfb0dd29692a45765669
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-11-25 14:42:56 +0900

    15917

--- BaekJoon_python/15917.py
+++ BaekJoon_python/15917.py
@@ -2,7 +2,6 @@ import sys
 for _ in range(int(input())):
     a = int(sys.stdin.readline())
     t = int(bin(a)[2:])
-    if t&(-t) == a:
-        print(1)
-    else:
-        print(0)
+    
+    if t&(-t) == a : print(1)
+    else : print(0)
