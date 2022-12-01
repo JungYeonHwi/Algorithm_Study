e99fe19dc9a84279017eea88ed4c29a002a26773
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-01 16:45:47 +0900

    12813

---
+++ BaekJoon_python/12813.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+a, b = int(input(),2), int(input(),2)
+n = 100000
+mask = 2 ** n - 1
+
+print(f'{a&b:0{n}b}')
+print(f'{a|b:0{n}b}')
+print(f'{a^b:0{n}b}')
+print(f'{mask-a:0{n}b}')
+print(f'{mask-b:0{n}b}')
