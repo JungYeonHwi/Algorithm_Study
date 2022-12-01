c4816711c338b7865951bfe23a7e28b9f51ae3dd
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-01 15:48:34 +0900

    25501

--- BaekJoon_python/25501.py
+++ BaekJoon_python/25501.py
@@ -1,14 +1,17 @@
-def recursion(s, l, r):
+import sys
+input = sys.stdin.readline
+
+def recursion(s, l, r) :
+    global cnt 
+    cnt += 1
+    
     if l >= r : return 1
     elif s[l] != s[r] : return 0
     else: return recursion(s, l+1, r-1)
 
-def isPalindrome(s):
+def isPalindrome(s) :
     return recursion(s, 0, len(s)-1)
 
-n = int(input())
-
-for _ in range(n) : 
-    str = input()
-    
-    print(isPalindrome(str))
+for _ in range(int(input())) :
+    cnt = 0
+    print(isPalindrome(input().rstrip()), cnt)
