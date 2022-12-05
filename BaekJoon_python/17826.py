40c5bb8b5e136b5ccdf68a1893ede2211963c01c
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-05 18:51:19 +0900

    17826

--- BaekJoon_python/17826.py
+++ BaekJoon_python/17826.py
@@ -1,10 +1,9 @@
-import sys
-n, m = map(int, sys.stdin.readline().split())
-cnt = []
-for i in range(m) :
-    a, b = map(int, sys.stdin.readline().split())
-    cnt.append(a)
-    cnt.append(b)
- 
-for i in range(1, n+1) :
-    print(cnt.count(i))
+tmp = (list(map(int, input().split())))
+ans = tmp.index(int(input())) + 1
+if ans < 6 : print("A+")
+elif ans < 16 : print("A0")
+elif ans < 31 : print("B+")
+elif ans < 36 : print("B0")
+elif ans < 46 : print("C+")
+elif ans < 49 : print("C0")
+else : print("F")
