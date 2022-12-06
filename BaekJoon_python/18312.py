f7f4c9ebbdf60854a3b8b8244f46ab20924c852a
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-06 13:45:51 +0900

    18312

---
+++ BaekJoon_python/18312.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+count = 0
+k = str(k)
+
+for i in range(n+1) :
+    if i < 10 :
+        i = '0' + str(i)
+    for j in range(60) :
+        if j < 10 :
+            j = '0' + str(j)
+        for t in range(60) :
+            if t < 10 :
+                t = '0' + str(t)
+            if k in str(i) + str(j) + str(t):
+                count += 1
+print(count)
