b265ccbf04dbfbd4898fe9e69fa1486b417a69e4
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-06 13:47:05 +0900

    16676

---
+++ BaekJoon_python/16676.py
@@ -0,0 +1,12 @@
+N = int(input())
+card = 1
+
+if N <= 10 : print(1)
+    
+else :
+    while N >= card :
+        card = str(card)
+        card += '1'
+        card = int(card)
+    
+    print(len(str(card//10)))
