a27ff1ca3533253948a526fead47c1da1ac23105
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-06 14:02:49 +0900

    1672

---
+++ BaekJoon_python/1672.py
@@ -0,0 +1,14 @@
+n = int(input())
+dna = list(input().rstrip())
+itp = {"AG":"C", "AC":"A", "AT":"G", "GC":"T", "GT":"A", "CT":"G"}
+
+a, b = "", dna.pop()
+for _ in range(n-1) :
+    a = dna.pop()
+    if a == b : continue
+        
+    if a+b in itp :
+        b = itp[a+b]
+    else :
+        b = itp[b+a]
+print(b)
