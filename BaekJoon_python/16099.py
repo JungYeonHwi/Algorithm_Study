25096ad3b3660433f7755670c97cba36c8186373
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-06 12:18:44 +0900

    16099

---
+++ BaekJoon_python/16099.py
@@ -0,0 +1,6 @@
+for _ in range(int(input())) : 
+    a, b, c, d = map(int, input().split())
+    
+    if a * b > c * d : print("TelecomParisTech")
+    elif a * b < c * d : print("Eurecom")
+    else : print("Tie")
