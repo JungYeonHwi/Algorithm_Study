352281c51babc5ebead59cf2c500c1f66132a943
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-06 23:25:19 +0900

    24724

---
+++ BaekJoon_python/24724.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for i in range(1, t+1) : 
+    n = int(input())
+    for _ in range(n+1) : 
+        a, b = map(int, input().split())
+        
+    print("Material Management " + str(i))
+    print("Classification ---- End!")
