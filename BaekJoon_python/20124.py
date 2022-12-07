e529432bce8e23ed5718a2b452b7a38b66f3b89c
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-07 15:36:19 +0900

    20124

---
+++ BaekJoon_python/20124.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+answer = []
+m = 0
+
+for _ in range(n) : 
+    a, b = map(str, input().split())
+    answer.append([a, int(b)])
+    
+    if int(b) > m : m = int(b)
+    
+    answer.sort(key = lambda x : x[1], reverse=True)
+    
+    name = []
+    for i, j in answer : 
+        if j == m : name.append(i)
+        
+    name.sort()
+    print(name[0])
