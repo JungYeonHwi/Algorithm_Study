e06a2e72e7ff9e340c6766b3ee2ad485826ddcb3
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-07 14:53:02 +0900

    17838

--- BaekJoon_python/17838.py
+++ BaekJoon_python/17838.py
@@ -1,7 +1,6 @@
 for _ in range(int(input())):
     s = input()
-    if len(s) == 7 : 
-        if s[0] == s[1] and  s[1] != s[2] and s[2] == s[3] and s[3] != s[4] and s[4] != s[5] and s[5] == s[6] :
-            print(1)
-        else : print(0)
-    else : print(0)
+    if len(set(s)) == 2 and len(s) == 7 and s[0] == s[1] and  s[1] != s[2] and s[2] == s[3] and s[3] != s[4] and s[4] != s[5] and s[5] == s[6] : answer = 1
+    else : answer = 0
+    
+    print(answer)
