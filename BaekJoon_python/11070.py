14a0c3e8a978fe2492e4e324dc6507b6ee1abc4d
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-08 16:54:35 +0900

    11070

--- BaekJoon_python/11070.py
+++ BaekJoon_python/11070.py
@@ -1,11 +1,18 @@
-for _ in range(int(input())) :
-    s = input().lower()
-    tmp = [1] * 26
-    for i in s :
-        if ord(i) > 96 and ord(i) < 123 : tmp[ord(i)-97] = 0
-    if sum(tmp) == 0 : print("pangram")
-    else :
-        print("missing ", end='')
-        for i in range(len(tmp)) :
-            if tmp[i] == 1 : print(chr(i+97), end='')
-        print()
+for _ in range(int(input())) : 
+    n, m = map(int, input().split())
+    score = [[0, 0] for _ in range(n)]
+    answer = []
+    
+    for i in range(m) : 
+        a, b, p, q = map(int, input().split())
+        score[a-1][0] += p
+        score[a-1][1] += q
+        score[b-1][0] += q
+        score[b-1][1] += p
+    
+    for j in range(n) : 
+        if score[j][0] == 0 and score[j][1] == 0 : answer.append(0)
+        else : answer.append(1000 * score[j][0] ** 2 / (score[j][0] ** 2 + score[j][1] ** 2))
+        
+    print(int(max(answer)))
+    print(int(min(answer)))
