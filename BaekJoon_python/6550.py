b0b74cc05d4f5e334e6b193ff9ab7e16bc934a98
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-09 18:56:00 +0900

    6550

---
+++ BaekJoon_python/6550.py
@@ -0,0 +1,18 @@
+while 1 :
+    try :
+        s, t = input().split()
+        
+        value = 0
+        flag = 0
+        
+        for i in range(len(t)) :
+            if t[i] == s[value] : 
+                value += 1
+                if value == len(s) : 
+                    flag = 1
+                    break
+
+        if flag == 1 : print('Yes')
+        else : print('No')
+
+    except : break
