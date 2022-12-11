1855822bbec326a07eb417e4b12f9a76ba2d948e
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-11 19:55:18 +0900

    2622

---
+++ BaekJoon_python/2622.py
@@ -0,0 +1,12 @@
+t = int(input())
+answer = 0
+
+for i in range(1, t) : 
+    for j in range(i, t) : 
+        value = t - i - j
+        
+        if j > value : break
+        if i + j > value : answer += 1
+        
+        
+print(answer)
