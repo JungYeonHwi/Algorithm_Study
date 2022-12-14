92f2b65078e7de819bc2a020ca0181f70228ee0e
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-14 16:16:02 +0900

    11652

---
+++ python/BaekJoon/11652.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+dic = {}
+
+for i in range(n) :
+    card = int(input())
+    if card in dic :
+        dic[card] += 1
+    else :
+        dic[card] = 1
+
+result = sorted(dic.items(), key = lambda x : (-x[1],x[0]))
