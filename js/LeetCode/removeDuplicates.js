05e83e6a529b1c65a67095d27dc12bcd0ddb2cc3
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-15 21:20:30 +0900

    removeDuplicates

---
+++ js/LeetCode/removeDuplicates.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+  if (nums.length == 0) {
+    return 0;
+  }
+
+  let i = 0;
+
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[j] != nums[i]) {
+      i += 1;
+      nums[i] = nums[j];
+    }
+  }
+
+  return i + 1;
+};
