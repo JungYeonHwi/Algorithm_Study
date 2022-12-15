ec3f95b6b981eb2c71a161e7cc3f9ac9dfc4d46b
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-15 15:20:07 +0900

    13116

---
+++ python/BaekJoon/13116.py
@@ -0,0 +1,14 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+for _ in range(n):
+    a, b = map(int, sys.stdin.readline().split())
+
+    while 1 :
+        if a == b :
+            print(a * 10)
+            break
+        
+        if a > b : a //= 2
+        else : b //= 2
