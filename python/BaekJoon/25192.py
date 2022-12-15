974ec9d6478679e4ee3e59fd51852be9017dc087
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-15 15:18:23 +0900

    25192

---
+++ python/BaekJoon/25192.py
@@ -0,0 +1,13 @@
+gomgom = set()
+cnt = 0
+
+for _ in range(int(input())):
+    word = input()
+
+    if word != 'ENTER' :
+        if word not in gomgom :
+            cnt += 1
+            gomgom.add(word)
+    elif word == 'ENTER' : gomgom.clear()
+            
+print(cnt)
