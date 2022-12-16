c7a60dd6f6871b0816bc9a91798d126678e78872
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-16 18:36:04 +0900

    2512

--- python/BaekJoon/2512.py
+++ python/BaekJoon/2512.py
@@ -1,20 +1,19 @@
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-budget = list(map(int, input().split()))
-M = int(input())
+cities = list(map(int, input().split()))
+budgets = int(input())
+start, end = 0, max(cities)
 
-start, end = 0, max(budget)
-total = 0
-
-if sum(budget) >= end :
-	print(max(budget))
-else :
-    while start <= end :
-        mid = (start+end) // 2
-
-        total = 0
-        for i in budget :
-            total += min(mid, i)
-
-        if total > M : end = mid - 1
-        else : start = mid + 1
-    print(mid)
+while start <= end:
+    mid = (start+end) // 2
+    total = 0
+    for i in cities :
+        if i > mid :
+            total += mid
+        else :
+            total += i
+    if total <= budgets : start = mid + 1
+    else : end = mid - 1
+print(end)
