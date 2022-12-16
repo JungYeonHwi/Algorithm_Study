77f95cbe612065173c04339301339a8538c0a446
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-16 18:29:41 +0900

    11582

---
+++ python/BaekJoon/11582.py
@@ -0,0 +1,12 @@
+N = int(input())
+c = list(map(int, input().split()))
+k = int(input())
+
+index = N // k
+arr = []
+
+for i in range(0, N, index) :
+    arr = c[i:i+index]
+    arr.sort()
+    for j in arr :
+        print(j, end=' ')
