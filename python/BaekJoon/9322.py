c7900f43c332b7b0797810059ef8912928cff9f1
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-16 18:31:31 +0900

    9322

---
+++ python/BaekJoon/9322.py
@@ -0,0 +1,16 @@
+for _ in range(int(input())):
+    lineLength = int(input())
+    l1 = input().split()
+    l2 = input().split()
+
+    indexList = []
+    for item in l1 :
+        indexList.append(l2.index(item))
+
+    secret = input().split()
+
+    resultList = []
+
+    for index in indexList :
+        resultList.append(secret[index])
+    print(*resultList)
