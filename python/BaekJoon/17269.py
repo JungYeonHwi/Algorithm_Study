a0de5c2d38ac737999ad52b117f622652f0ae39c
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-17 16:33:30 +0900

    17269

---
+++ python/BaekJoon/17269.py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split())
+A, B = input().split()
+alp = [3,2,1,2,4,3,1,3,1,1,3,1,3,2,1,2,2,2,1,2,1,1,1,2,2,1]
+AB = ""
+end = min(N, M)
+
+for i in range(end) :
+    AB += A[i] + B[i]
+AB += A[end:] + B[end:]
+
+lst = [alp[ord(i) - ord('A')] for i in AB]
+
+for i in range(N + M - 2) :
+    for j in range(N + M - 1 - i) :
+        lst[j] += lst[j+1]
+        
+print("{}%".format(lst[0] % 10 * 10 + lst[1] % 10))
