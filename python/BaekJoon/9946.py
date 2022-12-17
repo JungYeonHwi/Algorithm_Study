23029fb59cffd9847f9a2526c0545f5d067f8c11
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-17 16:35:06 +0900

    9946

---
+++ python/BaekJoon/9946.py
@@ -0,0 +1,13 @@
+i = 1
+ 
+while 1 :
+    a = input()
+    b = input()
+    if a == "END" and b == "END" : break
+    else :
+        A = sorted(list(a))
+        B = sorted(list(b))
+        if A == B : print("Case", str(i)+ ": same")
+        elif A != B : print("Case", str(i)+ ": different")
+            
+        i += 1
