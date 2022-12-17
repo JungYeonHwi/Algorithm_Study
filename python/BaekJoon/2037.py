6701a8ed61f71642c10b0887e471b68a8a03907e
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-17 16:39:51 +0900

    2037

---
+++ python/BaekJoon/2037.py
@@ -0,0 +1,20 @@
+keyboard = [[], ['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I'], ['J', 'K', 'L'], ['M', 'N', 'O'],
+                ['P', 'Q', 'R', 'S'], ['T', 'U', 'V'], ['W', 'X', 'Y', 'Z']]
+
+p, w = map(int, input().split())
+text = input()
+
+total = 0
+past = ''
+for i in range(len(text)) :
+    if text[i] == ' ' :
+        total += p
+        past = ''
+    for j in range(len(keyboard)) :
+        for k in range(len(keyboard[j])) :
+            if text[i] == keyboard[j][k] :
+                if j == past :
+                    total += w
+                total += (1+k) * p
+                past = j
+print(total)
