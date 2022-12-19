56b1bae758afc752965e9145687acc529b7fb907
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-19 18:00:53 +0900

    2565

---
+++ python/BaekJoon/2565.py
@@ -0,0 +1,10 @@
+n = int(input())
+a = sorted([list(map(int, input().split())) for _ in range(n)])
+
+dp = [1] * n
+for i in range(n) :
+    for j in range(i) :
+        if a[i][1] > a[j][1] :
+            dp[i] = max(dp[i], dp[j] + 1)
+
+print(n - max(dp))
