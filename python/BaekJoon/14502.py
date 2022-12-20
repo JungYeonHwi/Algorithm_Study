f7396d95c576e169f0b5379d77a78fdb45d79ef1
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-20 23:55:14 +0900

    7569

--- python/BaekJoon/14502.py
+++ python/BaekJoon/14502.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 from collections import deque
 import copy
 
---
+++ python/BaekJoon/7569.py
@@ -0,0 +1,52 @@
+import sys
+from collections import deque
+
+m, n, h = map(int, input().split())
+
+matrix = [[list(map(int, sys.stdin.readline().split())) for _ in range(n)] for _ in range(h)]
+visited = [[[False]*m for _ in range(n)] for _ in range(h)]
+
+queue = deque()
+
+dx = [-1, 1, 0, 0, 0, 0]
+dy = [0, 0, -1, 1, 0, 0]
+dz = [0, 0, 0, 0, -1, 1]
+
+answer = 0
+
+def BFS() : 
+    while queue : 
+        x, y, z = queue.popleft()
+        
+        for i in range(6) :
+            nx = x + dx[i]
+            ny = y + dy[i]
+            nz = z + dz[i]
+            
+            if nx < 0 or nx >= h or ny < 0 or ny >= n or nz < 0 or nz >= m:
+                continue
+
+            if matrix[nx][ny][nz] == 0 and visited[nx][ny][nz] == False:
+                queue.append((nx, ny, nz))
+                matrix[nx][ny][nz] = matrix[x][y][z] + 1
+                visited[nx][ny][nz] = True
+                
+for i in range(h) : 
+    for j in range(n) : 
+        for k in range(m) : 
+            if matrix[i][j][k] == 1 and visited[i][j][k] == 0 :
+                queue.append((i, j, k))
+                visited[i][j][k] = True
+
+BFS()
+
+for i in matrix : 
+    for j in i : 
+        for k in j : 
+            if k == 0 : 
+                print(-1)
+                exit(0)
+                
+    answer = max(answer, max(j))
+    
+print(answer - 1)
