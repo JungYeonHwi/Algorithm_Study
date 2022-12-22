20a1ac0afec49e0d3d198c828f8d023d0d5ee234
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-22 16:21:50 +0900

    9996

---
+++ python/BaekJoon/9996.py
@@ -0,0 +1,13 @@
+import re, sys
+input = sys.stdin.readline
+n = int(input())
+s, e = input().rstrip().split("*")
+pt = re.compile( s + ".*" + e + "+")
+
+for i in range(n):
+    string = input().rstrip()
+    a = pt.search(string)
+    if a and a.group() == string :
+        print("DA")
+    else :
+        print("NE")
