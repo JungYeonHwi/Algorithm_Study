96b58e09371ad0e2d1178c8a2bd4476f8ccd39d4
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-22 16:59:22 +0900

    2023

---
+++ python/BaekJoon/2023.py
@@ -0,0 +1,19 @@
+def find(num) :
+    for i in range(2, int(int(num)**0.5)+1) :
+        if int(num) % i == 0 :
+            return
+            
+    if len(num) == n :
+        print(num)
+        return
+
+    for p in prime :
+        find(num+p)
+    
+n = int(input())
+
+start = ['2', '3', '5', '7']
+prime = ['1', '3', '7', '9']
+
+for s in start:
+    find(s)
