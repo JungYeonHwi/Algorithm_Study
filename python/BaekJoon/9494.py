5dbb09ccd0d01ea2352fa6d70a8d5bdf4e65a14d
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-27 13:59:29 +0900

    9494

---
+++ python/BaekJoon/9494.py
@@ -0,0 +1,26 @@
+import sys
+
+while 1 :
+    num = int(sys.stdin.readline().rstrip())
+
+    if num == 0:
+        exit()
+    
+    roll = 0
+    for i in range(num):
+        line = sys.stdin.readline().rstrip()
+        line += " "
+        
+        idx = []
+        for j in range(len(line)):
+            if line[j] == ' ':
+                idx.append(j)
+            
+        if i == 0:
+            roll = idx[0]
+            
+        for k in range(len(idx)):
+            if idx[k] >= roll:
+                roll = idx[k]
+                break
+    print(roll+1)
