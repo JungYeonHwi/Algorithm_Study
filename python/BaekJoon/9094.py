102ea17172c5e04c4b6ed88d7708702f69b8daeb
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-28 01:30:13 +0900

    9094

--- python/BaekJoon/9094.py
+++ python/BaekJoon/9094.py
@@ -1,10 +1,10 @@
 import sys
 input = sys.stdin.readline
 
-_max = 101
-table = [[0] * _max for _ in range(_max)]
-for a in range(1, _max-1):
-    for m in range(1, _max):
+maxValue = 101
+table = [[0] * maxValue for _ in range(maxValue)]
+for a in range(1, maxValue-1):
+    for m in range(1, maxValue):
         table[a][m] = table[a-1][m]
         for b in range(1, a):
             if not (a*a+b*b+m) % (a*b):
