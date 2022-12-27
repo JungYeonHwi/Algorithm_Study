8be7e7801e768208413ce63ee4b009ecf25a0b86
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2022-12-28 01:19:23 +0900

    9663

---
+++ python/BaekJoon/9663.py
@@ -0,0 +1,23 @@
+def adjacent(x) :
+    for i in range(x) : 
+        if row[x] == row[i] or abs(row[x] - row[i]) == x - i :
+            return False 
+    return True
+ 
+def DFS(x) :
+    global result
+ 
+    if x == N :
+        result += 1
+    else :
+        for i in range(N) : 
+            row[x] = i
+            if adjacent(x) : 
+                DFS(x + 1)
+ 
+N = int(input())
+row = [0] * N
+result = 0
+
+DFS(0)
+print(result)
