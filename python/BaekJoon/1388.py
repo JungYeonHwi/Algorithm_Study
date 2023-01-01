151c8be95a1f9e8ec2dc61a9d56112966637dc26
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-01-01 22:22:47 +0900

    1388

---
+++ python/BaekJoon/1388.py
@@ -0,0 +1,23 @@
+n,m = map(int, input().split())
+graph = [] 
+for _ in range(n):
+    graph.append(list(input()))
+
+count = 0
+for i in range(n) :
+    a = ''
+    for j in range(m) :
+        if graph[i][j] == '-' :
+            if graph[i][j] != a :
+                count += 1
+        a = graph[i][j]
+
+for j in range(m) :
+    a = ''
+    for i in range(n) :
+        if graph[i][j] == '|' :
+            if graph[i][j] != a :
+                count += 1
+        a = graph[i][j]
+ 
+print(count)
