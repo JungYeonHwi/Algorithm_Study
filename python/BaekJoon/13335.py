596e78ca029a12c395950db63793767158b31efa
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-01-04 14:44:51 +0900

    13335

--- python/BaekJoon/13335.py
+++ python/BaekJoon/13335.py
@@ -2,14 +2,19 @@ n, w, l = map(int, input().split())
 trucks = list(map(int, input().split()))
  
 bridge = [0] * w
-time = 0
+weight, time = 0, 0
  
-while bridge :
-    time += 1
-    bridge.pop(0)
-    if bridge :
-        if sum(bridge) + trucks[0] <= l :
-            bridge.append(trucks.pop(0))
-        else :
-            bridge.append(0)
+while 2 :
+    out = bridge.pop(0)  
+    weight -= out    
+ 
+    if trucks : 
+        if weight + trucks[0] <= l : 
+            bridge.append(trucks[0]) 
+            weight += trucks[0]    
+            trucks.pop(0)  
+        else : bridge.append(0)  
+    time += 1 
+ 
+    if not bridge : break    
 print(time)
