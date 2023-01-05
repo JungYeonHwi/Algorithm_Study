8d90e0f623b9cd39463f29b7ce7679979512aab8
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-01-05 20:40:29 +0900

    18429

---
+++ python/BaekJoon/18429.py
@@ -0,0 +1,18 @@
+def DFS(depth, t):
+    global cnt
+    if depth == N:
+        cnt += 1
+        return ;
+    for i in range(N):
+        if check[i] or t+nums[i]-K < 0:
+            continue
+        check[i] = 1
+        DFS(depth+1, t+nums[i]-K)
+        check[i] = 0
+        
+N, K = map(int, input().split())
+nums = list(map(int, input().split()))
+check, cnt = [0]*N, 0
+
+DFS(0, 0)
+print(cnt)
