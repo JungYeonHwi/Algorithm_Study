f87939f9646136ec378941709400f0ef84700985
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-01-05 16:13:26 +0900

    9465

--- python/BaekJoon/9465.py
+++ python/BaekJoon/9465.py
@@ -1,11 +1,23 @@
-T = int(input())
-for _ in range(T) :
+import sys
+input = sys.stdin.readline
+
+for i in range(int(input())):
     n = int(input())
-    dp = [list(map(int, input().split())) for _ in range(2)]
-    dp[0][1] += dp[1][0]
-    dp[1][1] += dp[0][0]
+    arr = [[0]*n,[0]*n]
+    dp = [[0]*n,[0]*n]
     
-    for i in range(2,n) :
-        dp[0][i] += max(dp[1][i-1], dp[1][i-2])
-        dp[1][i] += max(dp[0][i-1], dp[0][i-2])
+    for idx, var in enumerate(map(int,input().split())):
+        arr[0][idx] = var
+    for idx, var in enumerate(map(int,input().split())):
+        arr[1][idx] = var
+
+    dp[0][0] = arr[0][0]
+    dp[1][0] = arr[1][0]
+
+    dp[0][1] = dp[1][0] + arr[0][1]
+    dp[1][1] = dp[0][0] + arr[1][1]
+
+    for i in range(2, n):
+        dp[0][i] = max(dp[1][i-1] + arr[0][i], dp[1][i-2] + arr[0][i])
+        dp[1][i] = max(dp[0][i-1] + arr[1][i], dp[0][i-2] + arr[1][i])
     print(max(dp[0][n-1], dp[1][n-1]))
