40b7848eccda2ec6c1a1a3640aea17fff1eb0ae6
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-01-08 17:03:36 +0900

    2302

---
+++ python/BaekJoon/2302.py
@@ -0,0 +1,28 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+m = int(input().rstrip())
+
+vips = [int(input().rstrip()) for _ in range(m)]
+
+dp = [0] * (n+1)
+
+dp[0] = 1
+dp[1] = 1
+dp[2] = 2
+
+for i in range(3, n+1):
+    dp[i] = dp[i-1] + dp[i-2]
+
+answer = 1
+if m >= 1:
+    tmp = 0
+    for i in range(m):
+        answer *= dp[vips[i]-1-tmp]
+        tmp = vips[i]
+    answer *=dp[n-tmp]
+else:
+    answer = dp[n]
+print(answer)
