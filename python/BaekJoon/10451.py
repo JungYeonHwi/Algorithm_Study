68cb711828c61fdd0ac763bad57c6f92d9bc5783
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-01-08 17:09:10 +0900

    10451

---
+++ python/BaekJoon/10451.py
@@ -0,0 +1,24 @@
+T = int(input())
+
+def BFS(V):
+    queue = [V]
+    visited[V] = 1  
+    while queue:
+        V = queue[0]
+        queue.pop(0)
+        next = arr[V]
+        if visited[next] == 0 :
+            visited[next] = 1
+            queue.append(next)
+    return 1
+
+for i in range(T):
+    answer = 0
+    N = int(input())
+    arr = [0] + list(map(int, input().split()))
+    visited = [0] * (N + 1)
+
+    for i in range(1, N+1) :
+        if visited[i] == 0 :
+            answer += BFS(i)
+    print(answer)
