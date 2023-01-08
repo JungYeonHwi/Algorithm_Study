b46005313dcfc767c88dab403a2249fd6f30a248
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-01-08 17:05:43 +0900

    1124

--- python/BaekJoon/2302.py
+++ python/BaekJoon/2302.py
@@ -17,12 +17,13 @@ for i in range(3, n+1):
     dp[i] = dp[i-1] + dp[i-2]
 
 answer = 1
-if m >= 1:
+
+if m >= 1 :
     tmp = 0
-    for i in range(m):
+    for i in range(m) :
         answer *= dp[vips[i]-1-tmp]
         tmp = vips[i]
     answer *=dp[n-tmp]
-else:
+else :
     answer = dp[n]
 print(answer)
