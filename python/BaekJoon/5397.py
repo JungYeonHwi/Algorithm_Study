85d492bfa26c700f3f7098e4de47ccfefde09a97
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-01-09 12:50:27 +0900

    5397

---
+++ python/BaekJoon/5397.py
@@ -0,0 +1,21 @@
+t = int(input())
+
+for _ in range(t):
+    left = []
+    right = []
+    pwd = input()
+
+    for x in pwd:
+        if x == ">":
+            if right:
+                left.append(right.pop()) 
+        elif x=="<":
+            if left:
+                right.append(left.pop())
+        elif x=="-":
+            if left:
+                left.pop()
+        else:
+            left.append(x)
+
+    print("".join(left)+"".join(reversed(right)))
