c891d2b4c5802c2ced424869b5413d2ba296bda8
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-01-09 12:45:36 +0900

    2589

--- python/BaekJoon/2589.py
+++ python/BaekJoon/2589.py
@@ -1,39 +1,36 @@
-import sys
 from collections import deque
 
-input = sys.stdin.readline
-
-n, m = map(int,input().split())
-graph = []
-
-for _ in range(n):
-    graph.append(list(input().rstrip()))
-
-
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-
-def BFS(x, y, cnt):
-    value = -1
-    q = deque()
-    q.append((x,y,cnt))
-    visited[x][y] = 1
-    while q:
-        x, y, curCnt = q.popleft()
-        if value < curCnt:
-            value = curCnt
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 'L' and visited[nx][ny] == 0 :
-                q.append((nx,ny,curCnt+1))
-                visited[nx][ny] = 1
-    return value
-
-m = -1
+n, m = map(int, input().split())
+maps = []
 for i in range(n):
-    for j in range(m):
-        if graph[i][j] == 'L':
-            visited = [[0] * (m) for _ in range(n)]
-            m = m(m,BFS(i,j,0))
-print(m)
+  maps.append(list(input()))
+
+dx=[1,-1,0,0]
+dy=[0,0,1,-1]
+
+def BFS(i,j):
+  queue=deque()
+  queue.append((i,j))
+  visited=[[0]*m for _ in range(n)]
+  visited[i][j]=1
+  cnt=0
+  while queue:
+    x,y=queue.popleft()
+    for i in range(4):
+      nx=x+dx[i]
+      ny=y+dy[i]
+      if nx<0 or nx>=n or ny<0 or ny>=m:
+        continue
+      elif maps[nx][ny]=='L' and visited[nx][ny]==0:
+        visited[nx][ny]=visited[x][y]+1
+        cnt=max(cnt,visited[nx][ny])
+        queue.append((nx,ny))
+  return cnt-1
+
+result=0
+for i in range(n):
+  for j in range(m):
+    if maps[i][j]=='L':
+      result=max(result,BFS(i,j))
+
+print(result)
