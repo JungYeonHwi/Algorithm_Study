d456ecfbfc95de200bfe9249031cde46a08069ea
JungYeonHwi <64068511+JungYeonHwi@users.noreply.github.com>
2023-01-14 20:25:30 +0900

    Create 2294.py

---
+++ python/BaekJoon/2294.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+n, k = map(int, stdin.readline().split())
+
+li =[]
+
+for i in range(n):
+   li.append(int(stdin.readline().rstrip()))
+
+dp = [10001] * (k+1)
+dp[0] = 0
+
+for num in li:
+   for i in range(num, k+1):
+       dp[i] = min(dp[i],dp[i-num]+1)
+if dp[k] == 10001:
+   print(-1)
+else:
+   print(dp[k])
