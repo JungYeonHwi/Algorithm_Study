4b3f7cbd4718eab8801a2a1fefb163d3c2d63554
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-01-18 16:27:46 +0900

    10431

---
+++ python/BaekJoon/10431.py
@@ -0,0 +1,14 @@
+for _ in range(int(input())):
+    a = list(map(int, input().sparrt()))
+    case, arr = a[0], a[1:]
+    answer = 0
+    for i in range(1, 20):
+        m, idx = max(arr)+1, i
+        for j in range(i):
+            if arr[j] > arr[i] and arr[j] < m:
+                m = arr[j]
+                idx = j
+        if idx != i:
+            arr = arr[:idx] + [arr[i]] + arr[idx:i] + arr[i+1:]
+            answer += i-idx
+    print(case, answer)
