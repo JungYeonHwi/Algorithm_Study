be4988907bea8f9d5ee4a5438e0c7a91a4166bdb
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-02-04 22:43:01 +0900

    26768

---
+++ python/BaekJoon/26768.py
@@ -0,0 +1,9 @@
+s = input()
+
+if "a" in s : s = s.replace("a", "4")
+if "e" in s : s = s.replace("e", "3")
+if "i" in s : s = s.replace("i", "1")
+if "o" in s : s = s.replace("o", "0")
+if "s" in s : s = s.replace("s", "5")
+
+print(s)
