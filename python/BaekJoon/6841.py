eda19762c4ad42e89fc3c7e44abd9ffead65ea38
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-02-27 22:32:46 +0900

    6841

---
+++ python/BaekJoon/6841.py
@@ -0,0 +1,19 @@
+while 1 : 
+    s = input()
+
+    if s == "TTYL" : 
+        print("talk to you later")
+        break
+    else : 
+        if s == "CU" : print("see you")
+        elif s == ":-)" : print("I’m happy")
+        elif s == ":-(" : print("I’m unhappy")
+        elif s == ";-)" : print("wink")
+        elif s == ":-P" : print("stick out my tongue")
+        elif s == "(~.~)" : print("sleepy")
+        elif s == "TA" : print("totally awesome")
+        elif s == "CCC" : print("Canadian Computing Competition")
+        elif s == "CUZ" : print("because")
+        elif s == "TY" : print("thank-you")
+        elif s == "YW" : print("you’re welcome")
+        else : print(s)
