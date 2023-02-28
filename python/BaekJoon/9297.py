9f951ee9cd8fd09853f9489d677425ba9940b19d
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-02-28 14:03:00 +0900

    9297

--- python/BaekJoon/9297.py
+++ python/BaekJoon/9297.py
@@ -6,6 +6,7 @@ for i in range(1, n+1) :
     if a < b : value = str(a) + "/" + str(b)
     else : 
         if (str(a%b)) == "0" : value = str(a // b)
+        elif (str(a%b)) == "0" and str(a // b) == "0" : value = str(0)
         else : value = str(a // b) + " " + (str(a%b) + "/" + str(b))
     
     print("Case", str(i) + ":", value)
