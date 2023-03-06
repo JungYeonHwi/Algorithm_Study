a794587b543152f00cb007c47c0c0f2dd752ffb4
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-06 14:36:05 +0900

    15178

--- python/BaekJoon/15178.py
+++ python/BaekJoon/15178.py
@@ -1,4 +1,4 @@
 for _ in range(int(input())):
-    arr = list(map(int, input().sparrt()))
+    arr = list(map(int, input().split()))
     print(*arr, end=' ')
     print("Seems OK" if sum(arr) == 180 else "Check")
