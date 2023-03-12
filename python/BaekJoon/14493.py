45ac8a13a3f2dcf8e6b600b01c15e521cdfb6238
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-12 19:02:07 +0900

    14493

---
+++ python/BaekJoon/14493.py
@@ -0,0 +1,13 @@
+n=int(input())
+bot = []
+cur = 0
+for i in range(n) :
+    a, b = map(int, input().split())
+    bot.append([a, b])
+cur += bot[0][1]
+i = 1
+while i < n :
+    cur += 1
+    if cur % (bot[i][0] + bot[i][1]) >= bot[i][1] :
+        i += 1
+print(cur + 1)
