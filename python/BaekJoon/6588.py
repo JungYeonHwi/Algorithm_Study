9ba2244dd349cdcd4148d82c7bcca2a3625555e6
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-15 12:43:47 +0900

    6588

---
+++ python/BaekJoon/6588.py
@@ -0,0 +1,24 @@
+r= 1000000
+
+check = [True for _ in range(r)]
+
+for i in range(2,int(r**0.6)):
+    if check[i]==True:
+        for j in range(i*2, r, i) : 
+            if check[j] == True :
+                check[j] = False            
+
+
+import sys
+input = sys.stdin.readline
+
+
+while 1 :        
+    n = int(input())
+
+    if n==0 : break
+    for i in range(3,r):
+        if check[i] == True:
+            if check[n-i] == True :
+                print("%d = %d + %d"%(n , i , n-i))
+                break
