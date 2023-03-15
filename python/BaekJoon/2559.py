c1b0f1a1242435819f30e34d06acc3c0c78ff7f9
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-03-15 11:46:19 +0900

    2559

--- python/BaekJoon/2559.py
+++ python/BaekJoon/2559.py
@@ -1,12 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-result =[]
-
 n, k = map(int,input().split())
 a = list(map(int, input().split()))
 
-for i in range(n - k + 1):
-    result.append(sum(a[i:i+k]))
+result = []
+result.append(sum(a[:k]))
+
+for i in range(n - k):
+    result.append(result[i] - a[i] + a[k+i])
         
 print(max(result))
