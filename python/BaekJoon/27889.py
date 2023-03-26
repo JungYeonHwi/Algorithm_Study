05edadf134db6bb7b0e05a1fe3053b31f5737aa0
JungYeonHwi <64068511+JungYeonHwi@users.noreply.github.com>
2023-03-26 18:53:26 +0900

    Create 27889.py

---
+++ python/BaekJoon/27889.py
@@ -0,0 +1,6 @@
+s = input()
+
+if s == 'NLCS' : print('North London Collegiate School')
+if s == 'BHA' : print('Branksome Hall Asia')
+if s == 'KIS' : print('Korea International School')
+if s == 'SJA' : print('St. Johnsbury Academy')
