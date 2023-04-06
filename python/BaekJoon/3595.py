0c642dac0a738104c5eee555dfc0ee71d16d611a
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-06 21:02:39 +0900

    3595

--- python/BaekJoon/3595.py
+++ python/BaekJoon/3595.py
@@ -1,26 +1,20 @@
-import sys
-input = sys.stdin.readline
-
 n = int(input())
-kk, jj, ii = 0, 0, 0
-min = 1000001
 
+m = 987654321
+num = [0, 0, 0]
 
-for i in range(n) : 
-    for j in range(n) : 
-        if i * j > n : break
-        
-        for k in range(n) : 
+for i in range(1, n+1) : 
+    for j in range(1, i+1) : 
+        if i * j > n :  break
+
+        for k in range(1, j+1) : 
             if i * j * k > n : break
-            elif i * j * k < n : continue
-            else : 
-                size = i * j * 2 + i * k * 2 + j * k * 2
-                if size < min : 
-                    min = size
-                    kk = k
-                    jj = j
-                    ii = i
-                    
-                break
-            
-print(kk, jj, ii)
+            if i * j * k == n :
+                s = i * j + j * k + k * i;
+                if s < m : 
+                    m = sum
+                    num[0] = i
+                    num[1] = j
+                    num[2] = k
+
+print(*num)
