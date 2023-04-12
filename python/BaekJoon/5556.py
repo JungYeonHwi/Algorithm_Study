1621be3d8591fd0f7cfe5d3cf2b60b9acd880deb
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-12 22:02:28 +0900

    5556

---
+++ python/BaekJoon/5556.py
@@ -0,0 +1,20 @@
+N = int(input())
+K = int(input())
+tiles = [list(map(int, input().split())) for _ in range(K)]
+
+for i, j in tiles :
+    if i > (N + 1) / 2:
+        i = N+1-i
+    
+    if j > (N+1)/2:
+        j = N+1-j
+    
+    if i > j:
+        answer = j%3
+    else:
+        answer = i%3
+    
+    if answer == 0:
+        answer = 3
+    
+    print(answer)
