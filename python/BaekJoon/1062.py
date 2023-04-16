ce6f373f6f6c94c55d8b519c812caaf0c0e812f6
JungYeonHwi <64068511+JungYeonHwi@users.noreply.github.com>
2023-04-16 21:08:11 +0900

    Create 1062.py

---
+++ python/BaekJoon/1062.py
@@ -0,0 +1,42 @@
+from itertools import combinations 
+import sys
+n, k = map(int, input().split())
+answer = 0
+# a,n,t,i,c는 반드시 가르쳐야 함
+
+first_word = {'a','n','t','i','c'}
+
+remain_alphabet = set(chr(i) for i in range(97, 123)) - first_word
+data = [sys.stdin.readline().rstrip()[4:-4] for _ in range(n)]
+
+def countReadableWords(data, learned):
+   cnt = 0
+   for word in data:
+      canRead = 1
+      for w in word:
+          # 안배웠으니까 못읽는다.
+         if learned[ord(w)] == 0:
+            canRead = 0
+            break
+      if canRead == 1:
+         cnt += 1
+   return cnt
+
+if k >= 5:
+   learned = [0] * 123
+   for x in first_word:
+      learned[ord(x)] = 1
+
+   # 남은 알파벳 중에서 k-5개를 선택해본다.
+   for teach in list(combinations(remain_alphabet, k-5)):
+      for t in teach:
+         learned[ord(t)] = 1
+      cnt = countReadableWords(data, learned)
+
+      if cnt > answer:
+         answer = cnt
+      for t in teach:
+         learned[ord(t)] = 0
+   print(answer)
+else:
+   print(0)
