056b1ac8fb76cf69f27806461edbb18481942114
wjddusgnl676@naver.com <wjddusgnl676@naver.com>
2023-04-17 21:17:39 +0900

    26645

---
+++ python/BaekJoon/26645.py
@@ -0,0 +1,10 @@
+n = int(input())
+
+answer = 0
+
+if n >= 200 and n <= 205 : answer = 1
+if n >= 206 and n <= 217 : answer = 2
+if n >= 218 and n <= 228 : answer = 3
+if n >= 229 and n <= 239 : answer = 4
+
+print(answer)
