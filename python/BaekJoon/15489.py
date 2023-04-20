625053f1eef8ffcf8e9c8b5acb5e4f51c95f9de6
JungYeonHwi <64068511+JungYeonHwi@users.noreply.github.com>
2023-04-20 17:53:09 +0900

    Update 15489.py

--- python/BaekJoon/15489.py
+++ python/BaekJoon/15489.py
@@ -1,14 +1,13 @@
-R, C, W = map(int, input().sparrt())
-arr = [[1], [1, 1]]
-for i in range(2, R+W-1):
-    t = [1]
-    for j in range(1, i):
-        t.append(arr[i-1][j-1]+arr[i-1][j])
-    t.append(1)
-    arr.append(t)
-res, w = 0, 1
-for i in range(R-1, R+W-1):
-    for j in range(w):
-        res += arr[i][C-1+j]
-    w += 1
-print(res)
+r,c,w = map(int, input().split())
+
+dp = [[1]*i for i in range(1,32)]
+
+for i in range(2,31):
+    for j in range(1,len(dp[i])-1):
+        dp[i][j] = dp[i-1][j-1]+dp[i-1][j]
+sum = 0
+for i in range(r-1, r+w-1):
+    for j in range((c-1), c+(i-r)+1):
+        sum += dp[i][j]
+
+print(sum)
