9c054653b5ba0e5d2b5d84f4b7e27ad679a1a12a
JungYeonHwi <64068511+JungYeonHwi@users.noreply.github.com>
2023-08-12 23:16:11 +0900

    Create 16967.py

---
+++ python/BaekJoon/16967.py
@@ -0,0 +1,24 @@
+import sys
+h,w,x,y = list(map(int,sys.stdin.readline().split()))
+
+B_array = [ 0*(w+y) for _ in range(h+x)]
+
+# 배열 B
+for i in range(h+x):
+  B_array[i] = list(map(int,sys.stdin.readline().split()))
+
+# 배열 A 초기화
+A_array = [[0]*w for _ in range(h)]
+
+for i in range(h):
+  for j in range(w):
+    A_array[i][j] = B_array[i][j]
+  
+for i in range(x,h):
+  for j in range(y,w):
+    A_array[i][j] = B_array[i][j] - A_array[i-x][j-y]
+
+for i in range(h):
+  for j in range(w):
+    print(A_array[i][j],end=" ")
+  print("")
