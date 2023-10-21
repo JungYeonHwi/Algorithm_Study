bbb0637d8d22d2e258b5c6487c457d63489cb396
JungYeonHwi <64068511+JungYeonHwi@users.noreply.github.com>
2023-10-21 23:37:54 +0900

    Create 7453(1).py

---
+++ python/BaekJoon/7453(1).py
@@ -0,0 +1,28 @@
+n = int(input())
+
+result = 0
+A, B, C, D = [], [], [], []
+for _ in range(n):
+    a, b, c, d = map(int, input().split())
+    A.append(a)
+    B.append(b)
+    C.append(c)
+    D.append(d)
+
+ab = dict()
+for a in A:
+    for b in B:
+        v = a + b
+        if v not in ab.keys():
+            ab[v] = 1
+        else:
+            ab[v] += 1
+
+
+for c in C:
+    for d in D:
+        v = -1 * (c + d)
+        if v in ab.keys():
+            result += ab[v]
+
+print(result)
