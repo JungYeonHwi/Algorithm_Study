4df1fb4554377a9079d727122b60f21c13fe309e
JungYeonHwi <64068511+JungYeonHwi@users.noreply.github.com>
2023-12-09 15:38:36 +0900

    Create 13144.py

---
+++ python/BaekJoon/13144.py
@@ -0,0 +1,22 @@
+import sys
+from collections import defaultdict
+
+num_dict = defaultdict(list)
+input = sys.stdin.readline
+n = int(input().rstrip())
+
+num_list = list(map(int, input().split()))
+
+result = 0
+start, end = 0, 0
+seq = [False for _ in range(1000001)]
+while start < n and end < n:
+    if not seq[num_list[end]]:      # start부터 end까지 중복 숫자 없으면
+        seq[num_list[end]] = True
+        end += 1
+        result += (end - start)     # end를 포함하여 만들 수 있는 수열의 개수
+    else:
+        seq[num_list[start]] = False
+        start += 1
+
+print(result)
