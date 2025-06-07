9b85487b6003223c7fc56f2bee85804404916acf
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-07 17:17:56 +0900

    프로그래머스 숫자 비교하기

--- js/programmers/숫자 비교하기.js
+++ js/programmers/숫자 비교하기.js
@@ -1,11 +1,4 @@
 function solution(num1, num2) {
-  var answer = 0;
-
-  if (num1 == num2) {
-    answer = 1;
-  } else {
-    answer = -1;
-  }
-
+  var answer = num1 === num2 ? 1 : -1;
   return answer;
 }
