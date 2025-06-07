98b5bdefaa9b9f02329e1ce9c6c9095a0c864e5d
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-07 18:14:28 +0900

    프로그래머스 짝수는 싫어요

--- js/programmers/짝수는 싫어요.js
+++ js/programmers/짝수는 싫어요.js
@@ -1,11 +1,14 @@
 function solution(n) {
   var answer = [];
+  let arr = [];
 
-  for (let i = 1; i < n + 1; i++) {
-    if (i % 2 != 0) {
-      answer.push(i);
-    }
+  for (i = 0; i < n + 1; i++) {
+    arr.push(i);
   }
+
+  let newArr = arr.filter((item) => item % 2 !== 0);
+
+  answer = newArr.sort((a, b) => a - b);
 
   return answer;
 }
