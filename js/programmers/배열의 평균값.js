5aa1db4365a74036ae8af9f88c0249827a57092a
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-07 18:22:59 +0900

    프로그래머스 배열의 평균값

--- js/programmers/배열의 평균값.js
+++ js/programmers/배열의 평균값.js
@@ -1,9 +1,7 @@
 function solution(numbers) {
   var answer = 0;
 
-  let sum = numbers.reduce((a, b) => a + b, 0);
-
-  answer = sum / numbers.length;
+  answer = numbers.reduce((a, b) => a + b) / numbers.length;
 
   return answer;
 }
