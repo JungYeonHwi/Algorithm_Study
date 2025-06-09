8fe34d30e97293416ab838429d7458302f1df085
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-10 01:12:09 +0900

    프로그래머스 문자 반복 출력하기

--- js/programmers/문자 반복 출력하기.js
+++ js/programmers/문자 반복 출력하기.js
@@ -1,11 +1,9 @@
 function solution(my_string, n) {
   var answer = "";
-
-  for (let i = 0; i < my_string.length; i++) {
-    for (let j = 0; j < n; j++) {
+  for (i = 0; i < my_string.length; i++) {
+    for (t = 0; t < n; t++) {
       answer += my_string[i];
     }
   }
-
   return answer;
 }
