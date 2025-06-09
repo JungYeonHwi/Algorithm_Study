e86774ab727e11892f22685f113ce001a1489435
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-10 01:12:02 +0900

    프로그래머스 짝수 홀수 개수

--- js/programmers/짝수 홀수 개수.js
+++ js/programmers/짝수 홀수 개수.js
@@ -1,12 +1,9 @@
 function solution(num_list) {
   var answer = [0, 0];
 
-  for (let i = 0; i < num_list.length; i++) {
-    if (num_list[i] % 2 == 0) {
-      answer[0] += 1;
-    } else {
-      answer[1] += 1;
-    }
+  for (i = 0; i < num_list.length; i++) {
+    if (num_list[i] % 2 === 0) answer[0] += 1;
+    else answer[1] += 1;
   }
 
   return answer;
