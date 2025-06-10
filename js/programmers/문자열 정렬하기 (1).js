39a906427413ff75bf5b12bba4f16c7667168af5
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-11 07:49:59 +0900

    프로래머스 문열 정렬하기 (1)

--- js/programmers/문자열 정렬하기 (1).js
+++ js/programmers/문자열 정렬하기 (1).js
@@ -1,8 +1,13 @@
 function solution(my_string) {
-  var answer = my_string
-    .match(/[0-9]/g)
-    .map((str) => parseInt(str))
-    .sort((a, b) => a - b);
+  var answer = [];
+
+  for (i = 0; i < my_string.length; i++) {
+    if (!isNaN(my_string[i])) {
+      answer.push(Number(my_string[i]));
+    }
+  }
+
+  answer = answer.sort((a, b) => a - b);
 
   return answer;
 }
