5f07ecb2cd9fc9494e72912a2af984836b6ada2b
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-12 23:56:14 +0900

    프로래머스 숨어있는 숫자의 덧셈 (1)

--- js/programmers/숨어있는 숫자의 덧셈 (1).js
+++ js/programmers/숨어있는 숫자의 덧셈 (1).js
@@ -1,10 +1,11 @@
 function solution(my_string) {
   var answer = 0;
 
-  var answer = my_string
-    .match(/[0-9]/g)
-    .map((str) => parseInt(str))
-    .reduce((accumulator, current) => accumulator + current, 0);
+  for (i = 0; i < my_string.length; i++) {
+    if (!isNaN(my_string[i])) {
+      answer += Number(my_string[i]);
+    }
+  }
 
   return answer;
 }
