74318376a8fffa46c27238432ec96c3ec9a30a2d
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-13 00:52:19 +0900

    프로그래머스 삼각형의 완성조건 (2)

--- js/programmers/삼각형의 완성조건 (2).js
+++ js/programmers/삼각형의 완성조건 (2).js
@@ -1,23 +1,21 @@
 function solution(sides) {
   var answer = 0;
+  let arr = [];
 
-  let M = Math.max(...sides);
-  let m = Math.min(...sides);
+  let max = Math.max(...sides);
+  let min = Math.min(...sides);
 
-  // M이 가장 긴 변인 경우
-
-  for (let i = 1; i < M + 1; i++) {
-    if (i + m > M) {
-      answer += 1;
-    }
+  // 가장 긴 변이 max인 경우
+  for (let i = 0; i <= max; i++) {
+    if (i + min > max) arr.push(i);
   }
 
-  // 나머지 한 변이 가장 긴 변인 경우
-  let value = M + 1;
-  while (value < M + m) {
-    answer += 1;
-    value += 1;
+  // 가장 긴 변이 알수 없는 경우
+  for (let i = max; i < max + min; i++) {
+    arr.push(i);
   }
+
+  answer = new Set(arr).size;
 
   return answer;
 }
