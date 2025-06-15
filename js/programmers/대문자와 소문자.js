6b448201f50dd2967696786d47d10587a939fcb4
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-15 15:29:43 +0900

    프로그래머스 대문자와 소문자

--- js/programmers/대문자와 소문자.js
+++ js/programmers/대문자와 소문자.js
@@ -1,16 +1,21 @@
+// 대문자인지 확인하는 함수 작성
+function isUpperCase(str) {
+  return str === str.toUpperCase();
+}
+
+// 소문자인지 확인하는 함수 작성
+function isLowerCase(str) {
+  return str === str.toLowerCase();
+}
+
 function solution(my_string) {
   var answer = "";
 
-  for (let i = 0; i < my_string.length; i++) {
-    let data = my_string[i];
-
-    let up = data.toUpperCase();
-    let low = data.toLowerCase();
-
-    if (data == up) {
-      answer += data.toLowerCase();
-    } else if (data == low) {
-      answer += data.toUpperCase();
+  for (i = 0; i < my_string.length; i++) {
+    if (isLowerCase(my_string[i])) {
+      answer += my_string[i].toUpperCase();
+    } else {
+      answer += my_string[i].toLowerCase();
     }
   }
 
