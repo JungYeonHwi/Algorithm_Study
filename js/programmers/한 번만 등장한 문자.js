7b38242ef059798ad5be3872e860ffe672b80adf
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-15 15:52:09 +0900

    프로그래머스 한 번만 등장한 문자

--- js/programmers/한 번만 등장한 문자.js
+++ js/programmers/한 번만 등장한 문자.js
@@ -1,46 +1,19 @@
 function solution(s) {
   var answer = "";
 
-  let dict = {
-    a: 0,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
-    g: 0,
-    h: 0,
-    i: 0,
-    j: 0,
-    k: 0,
-    l: 0,
-    m: 0,
-    n: 0,
-    o: 0,
-    p: 0,
-    q: 0,
-    r: 0,
-    s: 0,
-    t: 0,
-    u: 0,
-    v: 0,
-    w: 0,
-    x: 0,
-    y: 0,
-    z: 0,
-  };
+  let arr = Array.from({ length: 26 }, (_, i) => 0);
 
-  for (let i = 0; i < s.length; i++) {
-    let data = s.charAt(i);
-
-    dict[data] += 1;
+  for (i = 0; i < s.length; i++) {
+    arr[Number(s[i].charCodeAt() - 97)] += 1;
   }
 
-  for (var key in dict) {
-    if (dict[key] == 1) {
-      answer += key;
+  let res = [];
+
+  for (index = 0; index < arr.length; index++) {
+    if (arr[index] === 1) {
+      res.push(String.fromCharCode(97 + index));
     }
   }
 
-  return answer;
+  return res.join("");
 }
