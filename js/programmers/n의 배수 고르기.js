532f5fb429555b439b643240cdef65df8851759c
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-15 16:14:26 +0900

    프로그래머스 n의 배수 고르기

--- js/programmers/n의 배수 고르기.js
+++ js/programmers/n의 배수 고르기.js
@@ -1,11 +1,4 @@
 function solution(n, numlist) {
-  var answer = [];
-
-  numlist.forEach(function (value) {
-    if (value % n == 0) {
-      answer.push(value);
-    }
-  });
-
+  var answer = numlist.filter((item) => item % n === 0 && item);
   return answer;
 }
