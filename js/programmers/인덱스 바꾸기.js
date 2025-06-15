5b5714b16bbaede876e569439820ec806816ae1d
JungYeonHwi <wjddusgnl676@naver.com>
2025-06-15 15:33:59 +0900

    프로그래머스 인덱스 바꾸기

--- js/programmers/인덱스 바꾸기.js
+++ js/programmers/인덱스 바꾸기.js
@@ -1,16 +1,10 @@
 function solution(my_string, num1, num2) {
-  var answer = "";
-
-  let strList = my_string.split("");
-
-  let value = strList[num1];
-
-  strList[num1] = strList[num2];
-  strList[num2] = value;
-
-  for (let i = 0; i < strList.length; i++) {
-    answer += strList[i];
-  }
-
+  var answer = [...my_string]
+    .map((item, index) => {
+      if (index === num1) return my_string[num2];
+      else if (index === num2) return my_string[num1];
+      else return item;
+    })
+    .join("");
   return answer;
 }
