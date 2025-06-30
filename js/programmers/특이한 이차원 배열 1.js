8ba7f5e121642d68f1957756e77020cf1acfc9f0
JungYeonHwi <wjddusgnl676@naver.com>
2025-07-01 00:46:15 +0900

    프로그래머스 특이한 이차원 배열 1

---
+++ js/programmers/특이한 이차원 배열 1.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = [];
+    
+    for (let i = 0; i < n; i++) {
+        let arr = [];
+        for (let j = 0; j < n; j++) {
+            if (i === j) arr.push(1);
+            else arr.push(0)
+        }
+        answer.push(arr)
+    }
+    
+    return answer;
+}
