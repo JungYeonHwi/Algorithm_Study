92d811cca9f3a2085d28fc5bdd84251306990342
JungYeonHwi <wjddusgnl676@naver.com>
2025-07-01 01:39:41 +0900

    프로그래머스 배열의 원소 삭제하기

---
+++ js/programmers/배열의 원소 삭제하기.js
@@ -0,0 +1,9 @@
+function solution(arr, delete_list) {
+  var answer = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!delete_list.includes(arr[i])) answer.push(arr[i]);
+  }
+
+  return answer;
+}
