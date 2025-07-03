49827a47e9ee99fb86dff826a7cc522c663beca8
JungYeonHwi <wjddusgnl676@naver.com>
2025-07-04 01:08:02 +0900

    조건에 맞게 수열 변환하기 1

---
+++ js/programmers/조건에 맞게 수열 변환하기 1.js
@@ -0,0 +1,8 @@
+function solution(arr) {
+  var answer = arr.map((item) => {
+    if (item >= 50 && item % 2 === 0) return item / 2;
+    else if (item < 50 && item % 2 !== 0) return item * 2;
+    else return item;
+  });
+  return answer;
+}
