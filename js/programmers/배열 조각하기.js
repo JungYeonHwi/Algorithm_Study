26fb5146a7a72fb492423f427ef0d278a92bbd53
JungYeonHwi <wjddusgnl676@naver.com>
2025-07-05 16:37:45 +0900

    배열 조각하기

---
+++ js/programmers/배열 조각하기.js
@@ -0,0 +1,10 @@
+function solution(arr, query) {
+  var answer = arr;
+
+  for (let i = 0; i < query.length; i++) {
+    if (i % 2 === 0) answer = answer.slice(0, query[i] + 1);
+    else answer = answer.slice(query[i]);
+  }
+
+  return answer;
+}
