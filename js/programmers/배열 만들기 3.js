206f5536cfacf69cb9d2669224e30c9d65345f11
JungYeonHwi <wjddusgnl676@naver.com>
2025-07-05 16:48:49 +0900

    배열 만들기 3

---
+++ js/programmers/배열 만들기 3.js
@@ -0,0 +1,11 @@
+function solution(arr, intervals) {
+  var answer = [];
+
+  for (let i = 0; i < intervals.length; i++) {
+    let a = intervals[i][0];
+    let b = intervals[i][1];
+    answer.push(...arr.slice(a, b + 1));
+  }
+
+  return answer;
+}
