8b332081233bf079f2d85cea8618ed94feba728f
JungYeonHwi <wjddusgnl676@naver.com>
2025-07-07 22:21:36 +0900

    조건 문자열

---
+++ js/programmers/조건 문자열.js
@@ -0,0 +1,11 @@
+const operations = {
+  ">=": (n, m) => n >= m,
+  "<=": (n, m) => n <= m,
+  ">!": (n, m) => n > m,
+  "<!": (n, m) => n < m,
+};
+
+function solution(ineq, eq, n, m) {
+  const op = operations[ineq + eq];
+  return Number(op(n, m));
+}
