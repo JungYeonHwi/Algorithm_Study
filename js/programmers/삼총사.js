510bdca58efc006d299b3ae058b69cfe4b405df0
JungYeonHwi <wjddusgnl676@naver.com>
2025-07-17 16:13:39 +0900

    삼총사

---
+++ js/programmers/삼총사.js
@@ -0,0 +1,12 @@
+function solution(number) {
+  var answer = 0;
+  for (let i = 0; i < number.length - 2; i++) {
+    for (let j = i + 1; j < number.length - 1; j++) {
+      for (let k = j + 1; k < number.length; k++) {
+        const sum = number[i] + number[j] + number[k];
+        if (sum === 0) answer++;
+      }
+    }
+  }
+  return answer;
+}
