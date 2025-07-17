489b1fccf2d234e09028c6d3e7389f99dece1715
JungYeonHwi <wjddusgnl676@naver.com>
2025-07-17 13:43:57 +0900

    두 개 뽑아서 더하기

---
+++ js/programmers/두 개 뽑아서 더하기.js
@@ -0,0 +1,13 @@
+function solution(numbers) {
+  var answer = [];
+
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length; j++) {
+      answer.push(numbers[i] + numbers[j]);
+    }
+  }
+
+  answer = [...new Set(answer)].sort((a, b) => a - b);
+
+  return answer;
+}
