0b834b39c25c0071fbe457e5140ccfc303edc326
JungYeonHwi <wjddusgnl676@naver.com>
2025-07-17 14:31:45 +0900

    최소직사각형

---
+++ js/programmers/최소직사각형.js
@@ -0,0 +1,10 @@
+function solution(sizes) {
+  const [hor, ver] = sizes.reduce(
+    ([h, v], [a, b]) => [
+      Math.max(h, Math.max(a, b)),
+      Math.max(v, Math.min(a, b)),
+    ],
+    [0, 0]
+  );
+  return hor * ver;
+}
