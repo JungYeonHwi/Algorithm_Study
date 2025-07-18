3855cf57436996554ccc02fd3a860b8e75575ccc
JungYeonHwi <wjddusgnl676@naver.com>
2025-07-18 17:47:40 +0900

    둘만의 암호

---
+++ js/programmers/둘만의 암호.js
@@ -0,0 +1,35 @@
+function solution(s, skip, index) {
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ].filter((c) => !skip.includes(c));
+
+  return s
+    .split("")
+    .map((a) => alphabet[(alphabet.indexOf(a) + index) % alphabet.length])
+    .join("");
+}
