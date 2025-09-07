5d52877fee69a46ca7d054c64f7de469746fbe3d
JungYeonHwi <wjddusgnl676@naver.com>
2025-09-07 17:07:51 +0900

    twoSum

--- js/LeetCode/twoSum.js
+++ js/LeetCode/twoSum.js
@@ -3,12 +3,10 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[j] == target - nums[i]) {
-        return [i, j];
-      }
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if (nums[i] + nums[j] === target) return [i, j]
+        }
     }
-  }
 };
