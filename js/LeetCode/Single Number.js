22a7d891f0eb9c877911a8e2859ff72e2ecc82fc
JungYeonHwi <wjddusgnl676@naver.com>
2025-09-09 22:59:23 +0900

    Single Number

---
+++ js/LeetCode/Single Number.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function (nums) {
+  if (nums.length === 1) return nums[0];
+
+  nums = nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length - 1; i += 2) {
+    if (nums[i] !== nums[i + 1]) return nums[i];
+  }
+
+  return nums[nums.length - 1];
+};
