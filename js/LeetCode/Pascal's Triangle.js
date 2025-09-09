1941ba4134a3c218bd9255e00e25e18151c5c1b8
JungYeonHwi <wjddusgnl676@naver.com>
2025-09-09 22:06:51 +0900

    Pascal's Triangle

---
+++ js/LeetCode/Pascal's Triangle.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function (numRows) {
+  if (numRows === 1) return [[1]];
+  if (numRows === 2) return [[1], [1, 1]];
+
+  let answer = [[0], [1], [1, 1]];
+  for (let i = 3; i < numRows + 1; i++) {
+    let temp = [1];
+    for (let k = 1; k < i - 1; k++) {
+      let val = answer[i - 1][k - 1] + answer[i - 1][k];
+      temp.push(val);
+    }
+    temp.push(1);
+    answer.push(temp);
+  }
+
+  answer = answer.slice(1);
+
+  return answer;
+};
