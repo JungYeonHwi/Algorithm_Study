f6e9cb810d63a22e020adaae4ef6f691c3c1da3a
JungYeonHwi <wjddusgnl676@naver.com>
2025-10-08 18:20:23 +0900

    합성수 찾기

--- js/programmers/합성수 찾기.js
+++ js/programmers/합성수 찾기.js
@@ -1,20 +1,19 @@
+function isPrime(n) {
+  for (let i = 2; i < Math.sqrt(n) + 1; i++) {
+    if (n % i === 0) return true;
+  }
+
+  return false;
+}
+
 function solution(n) {
   var answer = 0;
 
-  if (n == 1) {
-    answer = 0;
-  } else {
-    for (let i = 2; i < n + 1; i++) {
-      let cnt = 0;
-      for (let j = 2; j < i + 1; j++) {
-        if (i % j == 0) {
-          cnt += 1;
-        }
-      }
-      if (cnt > 1) {
-        answer += 1;
-      }
-    }
+  if (n === 1) return 0;
+  if (n === 2) return 0;
+
+  for (let i = 3; i <= n; i++) {
+    if (isPrime(i)) answer += 1;
   }
 
   return answer;
