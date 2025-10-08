c35a9abc09cb7fa7ad8fa63f017ac229458ded7a
JungYeonHwi <wjddusgnl676@naver.com>
2025-10-08 18:10:39 +0900

    진료 순서 정하기

--- js/programmers/진료 순서 정하기.js
+++ js/programmers/진료 순서 정하기.js
@@ -1,20 +1,13 @@
 function solution(emergency) {
-  var answer = [];
+  const copiedEmergency = [...emergency].sort((a, b) => b - a);
+  var answer = new Array(emergency.length).fill(0);
 
-  let emergencyCopy = emergency.slice();
+  let k = 1;
 
-  emergencyCopy.sort(function (a, b) {
-    return a - b;
-  });
-
-  emergencyCopy.reverse();
-
-  for (let j = 0; j < emergency.length; j++) {
-    for (let i = 0; i < emergencyCopy.length; i++) {
-      if (emergency[j] == emergencyCopy[i]) {
-        answer.push(i + 1);
-      }
-    }
+  for (let i = 0; i < copiedEmergency.length; i++) {
+    let val = emergency.indexOf(copiedEmergency[i]);
+    answer[val] = k;
+    k += 1;
   }
 
   return answer;
