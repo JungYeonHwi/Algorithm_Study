4b561969aeff79bd5d66c3dc90a55d753bdcc5a1
JungYeonHwi <wjddusgnl676@naver.com>
2025-10-09 16:36:10 +0900

    소인수분해

--- js/programmers/소인수분해.js
+++ js/programmers/소인수분해.js
@@ -1,16 +1,28 @@
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
+  }
+  if (num % 2 === 0) {
+    return num === 2 ? true : false;
+  }
+
+  const sqrt = parseInt(Math.sqrt(num));
+
+  for (let divider = 3; divider <= sqrt; divider += 2) {
+    if (num % divider === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
 function solution(n) {
   var answer = [];
 
-  let result = [];
-  let divisor = 2;
-
-  while (n >= 2) {
-    if (n % divisor === 0) {
-      result.push(divisor);
-      n = n / divisor;
-    } else divisor++;
+  for (let i = 2; i <= n; i++) {
+    if (n % i === 0 && isPrime(i)) answer.push(i);
   }
 
-  answer = [...new Set(result)];
   return answer;
 }
