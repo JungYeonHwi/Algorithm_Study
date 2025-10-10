842364230d78037a2b0716e0c6d0ddfe0e8fbfcb
JungYeonHwi <wjddusgnl676@naver.com>
2025-10-10 18:16:41 +0900

    접미사 배열

---
+++ js/programmers/접미사 배열.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+  var answer = [];
+
+  for (let i = 0; i < my_string.length; i++) {
+    answer.push(my_string.slice(i, my_string.length));
+  }
+
+  return answer.sort();
+}
